e072f650dfc02df7922223a20a3c46ccad8f64ff
fonsecaaso <mateusfonseca27@hotmail.com>
2023-06-27 23:58:54 -0300

    update publisher

--- deposit/publisher.js
+++ deposit/publisher.js
@@ -2,11 +2,12 @@ const kafka = require('kafka-node');
 const readline = require('readline');
 
 
-const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9043' });
 const Producer = kafka.Producer;
-const topic = 'topico';
+const topic = 'request';
 
 const producer = new Producer(client);
+
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,15 +21,12 @@ var recursiveAsyncReadLine = function () {
         operation: values[0],
         amount: values[1],
         name1: values[2],
-        name2: values.length==4? values[3]:''
+        name2: values.length==4? values[3]:'',
+        id: Math.floor(Math.random() * 100000000)
     };
 
     producer.send([{ topic: topic, messages: [JSON.stringify(message)] }], function (err, data) {
-        if (err) {
-        console.log('Erro: ' + err);
-        } else {
-          console.log();
-        }
+        if (err) console.log('Erro: ' + err);
     });
     recursiveAsyncReadLine();
   });
