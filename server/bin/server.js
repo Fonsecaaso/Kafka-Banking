9b24cef9f62f18839d5b3df6e4f9e53d65d16566
fonsecaaso <mateusfonseca27@hotmail.com>
2023-06-28 00:29:03 -0300

    add nginx

--- server/bin/server.js
+++ server/bin/server.js
@@ -4,7 +4,7 @@ import app from '../src/app.js'
 
 debug('nodestr:server');
 
-const port = normalizePort(process.env.PORT || '9053');
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port)
 
 const server = http.createServer(app);
